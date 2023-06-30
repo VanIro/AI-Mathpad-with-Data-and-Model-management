ad0f46a00848944137711e43607eb6033d82ef49
Gopal Baidawar <gopal.baidawar@proxmed.com.au>
2023-06-30 14:11:11 +0545

    July30: preserve rating of each data from the user separately in frontend

--- FrontEnd/mathpad_page/src/components/ratings.js
+++ FrontEnd/mathpad_page/src/components/ratings.js
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ratings.css";
 
 const RatingSelect = ({ selectedRating, onSelectedRating, id_num }) => {
   const onChange = (e) => {
     const rating = Number(e.target.value);
-    onSelectedRating(rating);
+    onSelectedRating(id_num, rating);
   };
+
   return (
     <ul className="rating">
-      {[1,2,3,4,5].map((num,i) => {
+      {[1, 2, 3, 4, 5].map((num, i) => {
         return (
           <li key={i}>
             <input
               type="radio"
               id={`num${i}_${id_num}`}
-              name="rating"
+              name={`rating_${id_num}`}
               value={num}
               onChange={onChange}
+              checked={selectedRating === num}
             />
             <label htmlFor={`num${i}_${id_num}`}>{num}</label>
           </li>
